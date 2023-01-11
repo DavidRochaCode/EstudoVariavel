eb39ee64dd8f5f3a0be12643c3a10645d65e19eb
David Rocha <manoeudavi3@gmail.com>
2023-01-10 21:35:03 -0300

    comitar assunto de Construtores

--- EstudoVariavel/src/construtores/ConstrutorTeste.java
+++ EstudoVariavel/src/construtores/ConstrutorTeste.java
@@ -3,7 +3,30 @@ package construtores;
 public class ConstrutorTeste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// Objeto com construtor padrão
+		ProdutoTestar sabao = new ProdutoTestar();
+		sabao.marca = "Nivea";
+		sabao.anoVencimento = 2022;
+		sabao.matriculaProduto = 333344;
+		sabao.numero = 2233;
+		sabao.preco = 2.50f;
+
+		// Objeto com o construtor de dois parâmetros >> o que foi definido lá na classe
+		// ProdutoTestar
+		ProdutoTestar cafe = new ProdutoTestar("Ouro verde", 2233); // Passar os dois parâmetros
+		cafe.preco = 5.00f;
+		cafe.matriculaProduto = 444;
+		cafe.numero = 44;
+
+		// Objeto com o construtor de dois parâmetros >> o que foi definido lá na classe
+		// ProdutoTestar
+		ProdutoTestar biscoito = new ProdutoTestar("Treloso", 2233, 2022); // Passar os dois parâmetros
+		biscoito.preco = 5.00f;
+		cafe.matriculaProduto = 444;
+
+		// Se clicar em cima do objeto , e apertar F3, você será levado até o construtor
+		// dele;
 
 	}
 
--- EstudoVariavel/src/construtores/ProdutoTestar.java
+++ EstudoVariavel/src/construtores/ProdutoTestar.java
@@ -1,5 +1,39 @@
 package construtores;
 
 public class ProdutoTestar {
+	
+	String marca;
+	int numero;
+	float preco;
+	int matriculaProduto;
+	int anoVencimento;
+	
+	
+	//Construtor default, será o objeto sabao >> Permite instanciar objetos sem passar os parâmetros
+		ProdutoTestar(){}
+	
+	//construtor inicializando todos os atributos
+	public ProdutoTestar(String marca, int numero, float preco, int matriculaProduto, int anoVencimento) {
+		this.marca = marca;
+		this.numero = numero;
+		this.preco = preco;
+		this.matriculaProduto = matriculaProduto;
+		this.anoVencimento = anoVencimento;
+	}
+	
+	
+	//construtor inicializando 2 atributos >> será o objeto café
+	
+	public ProdutoTestar(String marca, int numero) {
+		this.marca = marca;
+		this.numero = numero;
+		}
+	
+	//construtor inicializando 3 atributos
+		public ProdutoTestar(String marca, int numero, int anoVencimento) {
+			this.marca = marca;
+			this.numero = numero;
+			this.anoVencimento = anoVencimento;
+		}
 
-}
+	}
